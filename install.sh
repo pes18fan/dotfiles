cc78e837220b777fed831142e1cee18482360ddf
vezei <sadbhavadhikari@gmail.com>
2023-02-02 20:24:00 +0545

    [added] tmux config

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 # copying all dotfiles to home directory
 ln -s .config $HOME/.config
 ln -s .vimrc $HOME/.vimrc
+ln -s .tmux.conf $HOME/.tmux.conf
 
 echo "Basic setup complete! To fully complete the setup: change your default shell to fish, use the command :PlugInstall in vim."
 echo "Additionally, if you wish, install a NerdFont from https://nerdfonts.com to enhance the Starship shell prompt experience. This is entirely optional, and the prompt will not be broken if you don't."
