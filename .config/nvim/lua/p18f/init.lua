47a258d3ad7dd0a56f143c730d7b369468faa1eb
pes18fan <pes18fan@disr.it>
2024-03-10 17:43:53 +0545

    refactor: move lazy bootstrap code to init file

--- .config/nvim/lua/p18f/init.lua
+++ .config/nvim/lua/p18f/init.lua
@@ -3,6 +3,19 @@ require("p18f.set")
 require("p18f.lazy")
 require("p18f.neovide")
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- suppress annoying offset_encoding popup, whatever that is
 local notify = vim.notify
     vim.notify = function(msg, ...)
