c873a0fd9da03e84894a7500c8c85255051858e7
pes18fan <pes18fan@disr.it>
2024-03-29 00:45:32 +0545

    fix: oil not showing hidden files

--- .config/nvim/lua/p18f/plugins/oil.lua
+++ .config/nvim/lua/p18f/plugins/oil.lua
@@ -1,14 +1,13 @@
 return {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        default_file_explorer = true,
-        view_options = {
-            show_hidden = true,
-        },
-    },
     config = function()
-        require("oil").setup()
+        require("oil").setup({
+            default_file_explorer = true,
+            view_options = {
+                show_hidden = true,
+            },
+        })
 
         vim.keymap.set("n", "<leader>pv", ":Oil<CR>", { desc = "Open oil.nvim" })
     end,
