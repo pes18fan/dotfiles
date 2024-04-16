7e7065cfad9401b87540fa6d125859fcd0db4ea5
pes18fan <pes18fan@disr.it>
2024-04-16 13:37:02 +0545

    Revert "refactor: simplified lsp config"
    This reverts commit 27b912f5844052c558a2860475fcd7f9d68a0f32.

--- .config/nvim/lua/p18f/plugins/lsp.lua
+++ .config/nvim/lua/p18f/plugins/lsp.lua
@@ -1,84 +1,50 @@
 return {
-    "neovim/nvim-lspconfig",
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
     event = "VeryLazy",
     dependencies = {
         { "williamboman/mason.nvim" },
         { "williamboman/mason-lspconfig.nvim" },
+        { "neovim/nvim-lspconfig" },
         { "hrsh7th/nvim-cmp" },
         { "hrsh7th/cmp-nvim-lsp" },
-        { "hrsh7th/cmp-cmdline" },
         { "L3MON4D3/LuaSnip" },
-        { "j-hui/fidget.nvim" },
     },
-
     config = function()
+        local lsp = require("lsp-zero")
         local cmp = require("cmp")
-        local cmp_lsp = require("cmp_nvim_lsp")
 
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities()
-        )
+        lsp.on_attach(function(_, bufnr)
+            lsp.default_keymaps({ buffer = bufnr })
+        end)
 
-        require("fidget").setup({})
+        cmp.setup({
+            mapping = cmp.mapping.preset.insert({
+                ["<C-c>"] = cmp.mapping.confirm({ select = true }),
+            })
+        })
+
         require("mason").setup({})
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "clangd",
                 "tsserver",
                 "ols",
+                "html",
             },
             handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({ capabilities = capabilities })
-                end,
-
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" }
-                                }
-                            }
-                        }
-                    })
-                end,
+                lsp.default_setup,
             },
         })
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-c>"] = cmp.mapping.confirm({ select = true }),
-            }),
-            sources = cmp.config.sources(
-                {
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" }
-                }, 
-                {
-                    { name = "buffer" },
+        require("lspconfig").lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        -- Suppress annoying "undefined global `vim`" warning
+                        globals = { "vim" }
+                    }
                 }
-            )
-        })
-
-        vim.diagnostic.config({
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
             }
         })
     end,
