9d009f37d8f1e6199c5c7294022b68814068120a
pes18fan <pes18fan@disr.it>
2024-04-16 22:56:29 +0545

    feat(nvim): modify emmet snippet for responsive mobile ui

--- .config/nvim/lua/p18f/plugins/emmet.lua
+++ .config/nvim/lua/p18f/plugins/emmet.lua
@@ -1,4 +1,27 @@
 return {
     "mattn/emmet-vim",
-    event = "BufReadPost",
+    event = "BufReadPre",
+    config = function()
+        vim.g.user_emmet_settings = {
+            html = {
+                snippets = {
+                    ["!"] = [[
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+
+<body>
+</body>
+
+</html>
+]]
+                }
+            }
+        }
+    end,
 }
