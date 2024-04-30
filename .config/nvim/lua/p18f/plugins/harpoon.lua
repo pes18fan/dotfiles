43971c54121f509f60f0c43335fc0b8d5bea76df
pes18fan <pes18fan@disr.it>
2024-04-30 20:33:43 +0545

    refactor(nvim): remove conjure

--- .config/nvim/lua/p18f/plugins/harpoon.lua
+++ .config/nvim/lua/p18f/plugins/harpoon.lua
@@ -7,10 +7,7 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a",
-            function()
-                harpoon:list():add()
-            end,
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end,
             { desc = "Add current file to harpoon" })
         vim.keymap.set("n", "<leader>m", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
             { desc = "Open harpoon menu" })
