43bc3f55d367e074c44e9d832cd4c27692dacc52
pes18fan <pes18fan@disr.it>
2024-05-25 15:03:07 +0545

    chore: remove dart.lua from ftdetect

--- .config/nvim/ftdetect/dart.lua
+++
@@ -1,5 +0,0 @@
--- Couldn't get dartls to start automatically so here's a little hack
-vim.api.nvim_create_autocmd({ "BufRead", "BufEnter" }, {
-    pattern = { "*.dart" },
-    command = "LspStart dartls",
-})
