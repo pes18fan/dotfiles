8f42ded5e1b32593348ecc728002275d83ecb8f8
pes18fan <pes18fan@disr.it>
2026-08-29 21:48:38 +0545

    update some nvim plugins

--- .config/nvim/lua/p18f/plugins/catppuccin.lua
+++ .config/nvim/lua/p18f/plugins/catppuccin.lua
@@ -1,50 +1,38 @@
+-- Set colorscheme and turn on transparency
+---@param color string
+function Color(color)
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatTitle", { bg = "none" })
+end
+
 -- Return a plugin setup object for lazy.nvim for a particular theme
 ---@param url string
 ---@param name string
----@param config_fn function?
-local function lazyTheme(url, name, config_fn)
+---@param opts table
+local function lazyTheme(url, name, opts)
     return {
         url,
         name = name,
         priority = 1000,
+        opts = opts,
         config = function()
-            if config_fn ~= nil then
-                config_fn()
-            end
-            vim.cmd.colorscheme(name)
+            Color(name)
         end
     }
 end
 
-return lazyTheme("rebelot/kanagawa.nvim", "kanagawa", function()
-    require("kanagawa").setup({
-        transparent = true,
-        colors = {
-            theme = {
-                all = {
-                    ui = {
-                        bg_gutter = "none"
-                    }
-                }
-            }
+return lazyTheme("catppuccin/nvim", "catppuccin", {
+    transparent_background = true,
+    integrations = {
+        blink_cmp = {
+            style = "bordered"
         },
-        overrides = function(colors)
-            local theme = colors.theme
-            return {
-                NormalFloat = { bg = "none" },
-                FloatBorder = { bg = "none" },
-                FloatTitle = { bg = "none" },
-
-                -- Save an hlgroup with dark background and dimmed foreground
-                -- so that you can use it where your still want darker windows.
-                -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-                NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-                -- Popular plugins that open floats will link to NormalFloat by default;
-                -- set their background accordingly if you wish to keep them dark and borderless
-                LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            }
-        end,
-    })
-end)
+        fidget = true,
+        gitsigns = true,
+        harpoon = true,
+        mason = true
+    }
+})
